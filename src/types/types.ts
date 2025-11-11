3b65898c8bf4a276d854e83fbba0cf99ad3aafba
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-11-11 22:11:52 +0530

    Added types for the request body

---
+++ src/types/types.ts
@@ -0,0 +1,63 @@
+import { Request } from "express";
+
+export interface RegisterBody {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginBody {
+  email: string;
+  password: string;
+}
+
+export interface LogoutBody {
+  refreshToken: string;
+}
+
+export interface RefreshBody {
+  refreshToken: string;
+  userId: string;
+}
+
+export interface ForgotPasswordBody {
+  email: string;
+}
+
+export interface ResetPasswordBody {
+  token: string;
+  newPassword: string;
+}
+
+//2FA requests
+export interface Verify2FABody {
+  otpCode: string;
+  tempEncryptedSecret: string;
+  backupCodes: string[];
+}
+
+export interface Verify2FABackupBody {
+  backupCode: string;
+  userEmail: string;
+}
+
+export interface VerifyLoginOTPBody {
+  userId: string;
+  otpCode: string;
+}
+
+export interface VerifyLoginBackupBody {
+  userId: string;
+  backupCode: string;
+}
+
+export interface JwtPayload {
+  userId: number;
+  tokenVersion: number;
+  iat?: number;
+  exp?: number;
+}
+
+export interface AuthenticateRequest extends Request {
+  user: JwtPayload;
+}
